81b203b147fd3a707562101e4214e41216f3ce85
Jacob Clarke <jacob@bigfish.tv>
2015-06-22 23:31:08 +1000

    window resize adjusts pixi canvas. added some test drawing.

--- app/core/Renderer.js
+++ app/core/Renderer.js
@@ -1,6 +1,15 @@
+import $ from 'jquery';
 import PIXI from 'pixi.js/bin/pixi';
-import WindowListener from '../core/WindowListener';
-let windowListener = new WindowListener();
+
+let windowWidth = 0;
+let windowHeight = 0;
+
+let baseWidth = 640;
+let baseHeight = 480;
+
+let imageRatio = baseWidth/baseHeight;
+
+let startDrawX, startDrawY, endDrawX, endDrawY = 0;
 
 export default class Renderer {
 
@@ -16,9 +25,10 @@ export default class Renderer {
 
 	init() {
 
-		let windowSize = windowListener.getWindowSize();
-		console.log(windowSize);
-		this.renderer = PIXI.autoDetectRenderer(windowSize.width, windowSize.height, {
+		console.log('initing');
+		this.updateWindowSize();
+
+		this.renderer = PIXI.autoDetectRenderer(windowWidth, windowHeight, {
 			antialias: true,
 			backgroundColor: 0x000000
 		});
@@ -26,7 +36,88 @@ export default class Renderer {
 		document.getElementById('stage').appendChild(this.renderer.view);
 
 		this.stage = new PIXI.Container();
+
+
+		let _this = this;
+		$(window).unbind('resize').bind('resize', () => _this.windowResized() );
+
+
+	}
+
+	updateWindowSize() {
+
+		// console.log('updating window size');
+		windowWidth = $(window).width();
+		windowHeight = $(window).height();
+
+		if(windowWidth/windowHeight > imageRatio) {
+			
+			startDrawY = 0;
+			endDrawY = windowHeight;
+
+			let offsetSize = baseWidth/(baseHeight/windowHeight);
+
+			startDrawX = (windowWidth - offsetSize)/2;
+			endDrawX = windowWidth - (windowWidth - offsetSize)/2;
+
+		}else{
+
+			startDrawX = 0;
+			endDrawX = windowWidth;
+
+			let offsetSize = baseHeight/(baseWidth/windowWidth);
+
+			startDrawY = (windowHeight - offsetSize)/2;
+			endDrawY = windowHeight - (windowHeight - offsetSize)/2;
+
+		}
+	}
+	
+	windowResized() {	
 		
+		// console.log('window resized');
+		this.updateWindowSize();
+		this.renderer.resize(windowWidth, windowHeight);
+		this.drawBounds();
+	}
+
+
+	getWindowSize() {
+		return {
+			width: windowWidth,
+			height: windowHeight
+		}
+	}
+
+	drawBounds() {
+
+		this.clearStage();
+
+		let graphics = new PIXI.Graphics();
+		graphics.lineStyle(4, 0xFFFFFF, 1);
+
+		// X
+		graphics.moveTo(0,0)
+		graphics.lineTo(windowWidth, windowHeight);
+		graphics.moveTo(windowWidth, 0);
+		graphics.lineTo(0, windowHeight);
+
+		// Bounds
+		graphics.moveTo(startDrawX, startDrawY);
+		graphics.lineTo(endDrawX, startDrawY);
+		graphics.lineTo(endDrawX, endDrawY);
+		graphics.lineTo(startDrawX, endDrawY);
+		graphics.lineTo(startDrawX, startDrawY);
+
+		this.stage.addChild(graphics);
+		this.renderer.render(this.stage);
+
+	}
+
+	clearStage() {
+		for (let index in this.stage.children ) {
+			this.stage.removeChild(this.stage.children[index])
+		}
 	}
 
 
--- app/index.js
+++ app/index.js
@@ -1,24 +1,19 @@
+import $ from 'jquery';
 import Effects from './effects/index';
 import Renderer from './core/Renderer';
 import KinectStream from './core/KinectStream';
-import WindowListener from './core/WindowListener';
 
 console.log(Effects);
-
-const circle = new Effects.Circles1();
-circle.params = {adam: 'sucks'};
-circle.render();
 
 let imageStreamURL = '/images';
 let kinectStream = new KinectStream();
 kinectStream.startStream(imageStreamURL);
 
-let windowListener = new WindowListener().init();
-
-
 let renderer = new Renderer();
 renderer.init();
+renderer.drawBounds();
 
+console.log(renderer.getWindowSize())
 
 
 // setTimeout(() => kinectStream.stopStream(), 1500);
