1b8db2a62b9693eda1f71216d0d4e2e3a74fe28b
Jacob Clarke <jacob@bigfish.tv>
2015-06-25 13:36:46 +1000

    Silly import mistake

--- app/core/SocketListener.js
+++ app/core/SocketListener.js
@@ -1,9 +1,9 @@
 import SocketUtil from '../utils/SocketUtil';
-import EffectActionCreators from '../actions/EffectActionCreators';
+import * as EffectActionCreators from '../actions/EffectActionCreators';
 
 let socketUtil = new SocketUtil();
 
 socketUtil.listen('effectParam', function(data) {
 	console.log('Effect param update received', data);
-	EffectActionCreators.updateEffectParam(data)
+	EffectActionCreators.updateEffectParam(data);
 });
