abeada438a9e77d35840acf0b89b41349c3dc69b
Jacob Clarke <jacob@bigfish.tv>
2015-07-21 10:35:55 +1000

    Idea for structure of filter classes.

--- app-redux/filters/rgbSplit.js
+++ app-redux/filters/rgbSplit.js
@@ -1,7 +1,19 @@
 import PIXI from 'pixi.js';
 
 export default class rgbSplit {
+	getParamDefaults() {
+		return [
+			{type: 'range', group: false, name: 'amount', label: 'Amount', value: 0, min: 0, max: 50, step: 0.5},
+			{type: 'range', group: 'details', name: 'redX', label: 'Red Offset X', value: -1, min: -5, max: 5, step: 1},
+			{type: 'range', group: 'details', name: 'greenX', label: 'Green Offset X', value: 0, min: -5, max: 5, step: 1},
+			{type: 'range', group: 'details', name: 'blueX', label: 'Blue Offset X', value: 1, min: -5, max: 5, step: 1},
+			{type: 'range', group: 'details', name: 'redY', label: 'Red Offset Y', value: -1, min: -5, max: 5, step: 1},
+			{type: 'range', group: 'details', name: 'greenY', label: 'Green Offset Y', value: 0, min: -5, max: 5, step: 1},
+			{type: 'range', group: 'details', name: 'blueY', label: 'Blue Offset Y', value: 1, min: -5, max: 5, step: 1},
+		];
+	}
 	constructor() {
-
+		this.filter = new PIXI.filters.RGBSplitFilter();
+		return filter;
 	}
 }
